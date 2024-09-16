8b40d82b2745f02a6eab8cfa6bb69ee78aac5fe2
hirotaka <iwakihiroshinao@iwakihiroshinaos-MacBook-Pro.local>
2024-09-16 19:30:53 -0400

    test2

--- qctools/__init__.py
+++ qctools/__init__.py
@@ -1,1 +1,2 @@
-from qctools import config, test  # noqa: F401
+from qctools import config  # noqa: F401
+from .test import testing, another_function
--- qctools/test.py
+++ qctools/test.py
@@ -1,2 +1,10 @@
+# qctools/test.py
+
 def testing():
     print('success!')
+
+def another_function():
+    print('another function here')
+
+def third_function():
+    print('this is the third function')
