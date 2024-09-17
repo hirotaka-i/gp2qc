652bcbbfde54663de3061007010fd6d83e068688
hirotaka <iwakihiroshinao@iwakihiroshinaos-MacBook-Pro.local>
2024-09-16 20:09:47 -0400

    delete

---
+++ gp2qc/__init__.py
@@ -0,0 +1,2 @@
+from .test import testing, another_function
+from .processing import read_file_and_process  # Import your new function
--- qctools/__init__.py
+++
@@ -1,2 +0,0 @@
-from qctools import config  # noqa: F401
-from .test import testing, another_function
