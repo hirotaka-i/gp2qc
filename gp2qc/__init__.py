aba3c1715a47cdbef0f24754047d1ac4a5e34784
hirotaka-i <34422057+hirotaka-i@users.noreply.github.com>
2024-10-10 13:48:09 -0400

    Update __init__.py
    Load base_check too

--- gp2qc/__init__.py
+++ gp2qc/__init__.py
@@ -3,3 +3,4 @@ from .processing import GP2SampleManifesstProcessor
 from .consistency import StudyManifestHandler
 from .save_df_to_gdrive import save_df_to_gdrive
 from .remove_sample_ids import remove_sample_ids
+from .base_check import base_check
