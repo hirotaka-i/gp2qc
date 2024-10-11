a55bb6f05d71f999e5410e28b1e8f72724570bbb
hirotaka <iwakihiroshinao@iwakihiroshinaos-MacBook-Pro.local>
2024-10-11 16:44:24 -0400

    add sample_id function

--- gp2qc/__init__.py
+++ gp2qc/__init__.py
@@ -4,3 +4,4 @@ from .consistency import StudyManifestHandler
 from .save_df_to_gdrive import save_df_to_gdrive
 from .remove_sample_ids import remove_sample_ids
 from .base_check import base_check
+from .add_sample_ids import add_sample_ids
